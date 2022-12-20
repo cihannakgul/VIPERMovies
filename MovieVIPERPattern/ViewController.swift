37cd9b07d4114f514caf3e5b313ab903e8bdb277
Cihan <cihannakgul@gmail.com>
2022-12-20 16:10:58 +0300

    Initial Commit

---
+++ MovieVIPERPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieVIPERPattern
+//
+//  Created by cihan on 20.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
